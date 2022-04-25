1d4a6eab041e6e2267cb1cac8525be423ea6c670
edufdev <ofuentesm4@gmail.com>
2022-04-24 22:52:57 -0600

    arreglando el ultimo proyecto

--- Proyecto-Hangman/Hangman.py
+++ Proyecto-Hangman/Hangman.py
@@ -4,6 +4,18 @@ import os
 import sys
 from time import sleep
 
+def normalize(vocal): # It removes the accents of a string
+    replacements = (
+        ("á", "a"),
+        ("é", "e"),
+        ("í", "i"),
+        ("ó", "o"),
+        ("ú", "u"),
+        )
+    for a, b in replacements:
+        vocal = vocal.replace(a, b).replace(a.upper(), b.upper())
+    return vocal
+
 
 def hangman():
     print('    \n¡Excelente, empecemos el juego!\n')
@@ -15,6 +27,7 @@ def hangman():
     
     la_palabra = random.choice(words).upper()
     palabra_secreta = ['_' for letter in la_palabra]
+    return palabra_secreta
 
     vidas = 7
     puntos = 0
@@ -22,40 +35,13 @@ def hangman():
 
 
 def menu():
-    
-    menu = '''
-    menu: 
-
-    1) ¿Desea empezar a jugar?
-    2) ¿Desea salir del juego?
-    (1) o (2):
-    R:// '''
-
-    opciones = int(input(f'\t{menu}'))
-
-    try:
-        if opciones == 1:
-            return hangman()
-        if opciones == 2:
-            print('Adios')
-            time.sleep(1)
-        if opciones > 2:
-            print('Ingrese una opcion valida por favor')
-            time.sleep(2)
-            print('Intente otra vez')
-            time.sleep(2)
-            main()
-        if opciones <= 0:
-            print('Ingrese una opcion valida por favor')
-            time.sleep(2)
-            print('Intente otra vez')
-            time.sleep(2)
-            main()
-    except ValueError:
-        print('Ingrese un valor valido')
-        print('Intente otra vez')
-        time.sleep(2)
+    nuevo_intento = input('¿Deseas volver a jugar?: (s/n): ')
+    if nuevo_intento == 's':
+        os.system('clear')
         main()
+    else:
+        sys.exit()
+    
 
 
 def mecanografiar(texto):
@@ -63,6 +49,7 @@ def mecanografiar(texto):
         sleep(0.2)
         print(palabras, end=' ', flush=True)
 
+os.system('clear')
 print("\n")
 mecanografiar("Bienvenido  Al  Juego  Del  Ahorcado")
 print("\n")
@@ -77,15 +64,49 @@ print('''
     ''')
 print("\n")
 mecanografiar("Adivina la palabra secreta o muere")
-print("\n")
 mecanografiar("tienes 7 vidas, pierdes una vida cada que te equivocas si te quedas sin vidas pierdes")
 print("\n")
 
 
 
 def main():
-    menu()
-    time.sleep(2)
+    menu = '''
+    menu: Seleccione la opción
+
+    1) ¿Empezar a jugar?
+    2) ¿Salir del juego?
+    R:// '''
+
+    opciones = int(input(f'\t{menu}'))
+
+    try:
+        if opciones == 1:
+            return hangman()
+        if opciones == 2:
+            os.system('clear')
+            print('Adios')
+            time.sleep(1)
+            os.system('clear')
+        if opciones > 2:
+            print('Ingrese una opcion valida por favor')
+            time.sleep(2)
+            print('Intente otra vez')
+            time.sleep(2)
+            os.system('clear')
+            main()
+        if opciones < 1:
+            print('Ingrese una opcion valida por favor')
+            time.sleep(2)
+            print('Intente otra vez')
+            time.sleep(2)
+            os.system('clear')
+            main()
+    except ValueError:
+        print('Ingrese un valor valido')
+        print('Intente otra vez')
+        time.sleep(2)
+        os.system('clear')
+        main()
 
     
 
