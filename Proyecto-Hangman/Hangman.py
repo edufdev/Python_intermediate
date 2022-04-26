6fee84a80d335e82b15a6cd105bffb7f62ccea7f
edufdev <ofuentesm4@gmail.com>
2022-04-25 23:47:34 -0600

    Intentando arreglar un bug

--- Proyecto-Hangman/Hangman.py
+++ Proyecto-Hangman/Hangman.py
@@ -4,38 +4,30 @@ import os
 import sys
 from time import sleep
 
-def normalize(vocal): # It removes the accents of a string
-    replacements = (
-        ("á", "a"),
-        ("é", "e"),
-        ("í", "i"),
-        ("ó", "o"),
-        ("ú", "u"),
-        )
-    for a, b in replacements:
-        vocal = vocal.replace(a, b).replace(a.upper(), b.upper())
-    return vocal
 
-
-def hangman():
-    print('    \n¡Excelente, empecemos el juego!\n')
-
-    words = [] 
-    with open("./data.txt", "r", encoding="utf-8") as f:
-        for line in f: 
+def read_words(): # Abrir y leer archivo donde estan los datos
+    words= []
+    with open('./data/data.txt', 'r', encoding='utf-8') as f:
+        for line in f:
             words.append(line.strip().upper())
-    
-    la_palabra = random.choice(words).upper()
-    palabra_secreta = ['_' for letter in la_palabra]
-    return palabra_secreta
-
-    vidas = 7
-    puntos = 0
+    return words
 
 
+# def normalize(vocal): # Elimina los accentos
+#     replacements = (
+#         ("á", "a"),
+#         ("é", "e"),
+#         ("í", "i"),
+#         ("ó", "o"),
+#         ("ú", "u"),
+#     )
+#     for a, b in replacements:
+#         vocal = vocal.replace(a, b).replace(a.upper(), b.upper())
+#     return vocal
 
-def menu():
-    nuevo_intento = input('¿Deseas volver a jugar?: (s/n): ')
+
+def try_again(): #Menú para saber si desea volver a jugar
+    nuevo_intento = input('¿Deseas volver a jugar?: Si(s)/Salir(cualquier otra tecla): ')
     if nuevo_intento == 's':
         os.system('clear')
         main()
@@ -44,70 +36,73 @@ def menu():
     
 
 
-def mecanografiar(texto):
+def mecanografiar(texto): # Introducción en modo mecanografico
     for palabras in texto.split():
         sleep(0.2)
         print(palabras, end=' ', flush=True)
 
-os.system('clear')
 print("\n")
+os.system('clear')
 mecanografiar("Bienvenido  Al  Juego  Del  Ahorcado")
 print("\n")
-print('''  
+print('''\t  
  +----+
  |    |
  O    |
 /|\   |
 / \   |
       |
-    =====
-    ''')
+     =====''')
 print("\n")
 mecanografiar("Adivina la palabra secreta o muere")
-mecanografiar("tienes 7 vidas, pierdes una vida cada que te equivocas si te quedas sin vidas pierdes")
+print()
+mecanografiar("Pierdes una vida cada que te equivocas si te quedas sin vidas pierdes el juego")
 print("\n")
+time.sleep(2)
+os.system('clear')
 
 
 
-def main():
-    menu = '''
-    menu: Seleccione la opción
 
-    1) ¿Empezar a jugar?
-    2) ¿Salir del juego?
-    R:// '''
+def main(): # Inicio del programa
+    data = read_words()
+    chosen_word = random.choice(data)
+    word_list = [letter for letter in chosen_word]
+    underscore = ["__"] * len(word_list)
+    letter_index = {}
+    for i, letter in enumerate(chosen_word):
+        if not letter_index.get(letter):
+            letter_index[letter] = []
+        letter_index[letter].append(i)
 
-    opciones = int(input(f'\t{menu}'))
+    fails = 0
+    guessed_words = []
 
-    try:
-        if opciones == 1:
-            return hangman()
-        if opciones == 2:
-            os.system('clear')
-            print('Adios')
-            time.sleep(1)
-            os.system('clear')
-        if opciones > 2:
-            print('Ingrese una opcion valida por favor')
-            time.sleep(2)
-            print('Intente otra vez')
-            time.sleep(2)
-            os.system('clear')
-            main()
-        if opciones < 1:
-            print('Ingrese una opcion valida por favor')
-            time.sleep(2)
-            print('Intente otra vez')
-            time.sleep(2)
-            os.system('clear')
-            main()
-    except ValueError:
-        print('Ingrese un valor valido')
-        print('Intente otra vez')
-        time.sleep(2)
-        os.system('clear')
-        main()
+    while True:
+        print("Adivina la palabra!!!")
+        if fails == 1:
+            print(f"Tienes {fails} oportunid restante")
+        else:
+            print(f"Tienes {fails - 1} oportunidades restantes.")
+        print(fails)
+        print()
+        for element in underscore:
+            print(element + " ", end="")
+        print()
 
+
+        while True:
+            letter = input("Ingresa una letra: ").strip().upper()
+            if letter in guessed_words:
+                print("Ya intentaste con " + letter + ".\n intenta con otra")
+            else:
+                if letter.isalpha() == True and len(letter) == 1:
+                    guessed_words.append(letter)
+                    break
+                elif letter.isalpha() == True and len(letter) > 1:
+                    print("Solamente puedes ingresar una letra a la vez!")
+                else:
+                    print("Solo puedes ingresar letras")
     
 
 
